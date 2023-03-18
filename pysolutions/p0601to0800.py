296bc4b155bd7a123dd850c66026f743d244ea50
JingXu <jx_tdem@qq.com>
2023-03-18 14:24:39 +0800

    BS1 Day4 P744

--- pysolutions/p0601to0800.py
+++ pysolutions/p0601to0800.py
@@ -29,3 +29,16 @@ class Pro0601To0800:
                 return i
             lsum += nums[i]
         return -1
+
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        # 744.Find Smallest Letter Greater Than Target
+        if letters[-1] <= target:
+            return letters[0]
+        left, right = 0, len(letters) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if letters[mid] > target:
+                right = mid
+            elif letters[mid] <= target:
+                left = mid + 1
+        return letters[left]
--- tests/test_p0601to0800.py
+++ tests/test_p0601to0800.py
@@ -18,3 +18,9 @@ class TestP0601To0800(unittest.TestCase):
         self.assertEqual(self.sl.pivotIndex([1, 7, 3, 6, 5, 6]), 3)
         self.assertEqual(self.sl.pivotIndex([1, 2, 3]), -1)
         self.assertEqual(self.sl.pivotIndex([2, 1, -1]), 0)
+
+    def test_nextGreatestLetter(self):
+        # 744.Find Smallest Letter Greater Than Target
+        self.assertEqual(self.sl.nextGreatestLetter(["c", "f", "j"], "a"), "c")
+        self.assertEqual(self.sl.nextGreatestLetter(["c", "f", "j"], "c"), "f")
+        self.assertEqual(self.sl.nextGreatestLetter(["x", "x", "y", "y"], "z"), "x")
