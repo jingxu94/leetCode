07651a3922f3f58cdc5dee95c809871642f2cf3f
JingXu <jx_tdem@qq.com>
2023-03-21 19:43:27 +0800

    modified function for generating ListNode and TreeNode

--- pysolutions/utils.py
+++ pysolutions/utils.py
@@ -1,4 +1,6 @@
-# Definition for singly-linked list.
+from typing import List, Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -16,3 +18,29 @@ class Node:
     def __init__(self, val=None, children=None):
         self.val = val
         self.children = children
+
+
+def create_linked_list(nums: List[int]) -> Optional[ListNode]:
+    if not nums:
+        return None
+    head = ListNode(nums[0])
+    curr = head
+    for num in nums[1:]:
+        curr.next = ListNode(num)
+        curr = curr.next
+    return head
+
+
+def create_binary_tree(nums: List[Optional[int]]) -> Optional[TreeNode]:
+    def create_tree(index: int) -> Optional[TreeNode]:
+        if index >= len(nums) or nums[index] is None:
+            return None
+        val = nums[index]
+        if val is not None:
+            node = TreeNode(val)
+            node.left = create_tree(2 * index + 1)
+            node.right = create_tree(2 * index + 2)
+            return node
+        return None
+
+    return create_tree(0)
