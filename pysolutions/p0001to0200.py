b08a512f328a7484ce19d7e3a1a4b0c6592a61ff
JingXu <jx_tdem@qq.com>
2023-07-18 10:13:45 +0800

    P146

--- pysolutions/p0001to0200.py
+++ pysolutions/p0001to0200.py
@@ -2531,6 +2531,59 @@ class LRUCache:  # pragma: no cover
             self.cache.popitem(last=False)
 
 
+class LRUCache_v2:  # pragma: no cover
+    # 146.LRU Cache
+    class Node:
+        def __init__(self, key, val):
+            self.key = key
+            self.val = val
+            self.prev = None
+            self.next = None
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.head = self.Node(-1, -1)
+        self.tail = self.Node(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.m: Dict[int, Node] = {}
+
+    def addNode(self, newnode):
+        temp = self.head.next
+        newnode.next = temp
+        newnode.prev = self.head
+        self.head.next = newnode
+        temp.prev = newnode
+
+    def deleteNode(self, delnode):
+        prevv = delnode.prev
+        nextt = delnode.next
+        prevv.next = nextt
+        nextt.prev = prevv
+
+    def get(self, key: int) -> int:
+        if key in self.m:
+            resNode = self.m[key]
+            ans = resNode.val
+            del self.m[key]
+            self.deleteNode(resNode)
+            self.addNode(resNode)
+            self.m[key] = self.head.next
+            return ans
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.m:
+            curr = self.m[key]
+            del self.m[key]
+            self.deleteNode(curr)
+        if len(self.m) == self.cap:
+            del self.m[self.tail.prev.key]
+            self.deleteNode(self.tail.prev)
+        self.addNode(self.Node(key, value))
+        self.m[key] = self.head.next
+
+
 class MinStack:  # pragma: no cover
     # 155.Min Stack
     def __init__(self):
