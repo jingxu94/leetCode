5bc7d845b327988af7b1373465648ef17cd373d3
JingXu <jx_tdem@qq.com>
2023-08-05 12:29:38 +0800

    P95

--- pysolutions/p0001to0200.py
+++ pysolutions/p0001to0200.py
@@ -1634,6 +1634,29 @@ class Pro0001To0200:
         # _treenode_traver(root, self.ans)
         # return self.ans
 
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:  # pragma: no cover
+        # 95.Unique Binary Search Trees II
+        dp = [[] for _ in range(n + 1)]
+        dp[0].append(None)
+
+        for numberOfNodes in range(1, n + 1):
+            for i in range(1, numberOfNodes + 1):
+                j = numberOfNodes - i
+                for left in dp[i - 1]:
+                    for right in dp[j]:
+                        root = TreeNode(i, left, self.clone(right, i))
+                        dp[numberOfNodes].append(root)
+
+        return dp[n]
+
+    def clone(self, node: Optional[TreeNode], offset: int) -> Optional[TreeNode]:  # pragma: no cover
+        if not node:
+            return None
+        cloned_node = TreeNode(node.val + offset)
+        cloned_node.left = self.clone(node.left, offset)
+        cloned_node.right = self.clone(node.right, offset)
+        return cloned_node
+
     def numTrees(self, n: int) -> int:
         # 96.Unique Binary Search Trees
         # Initialize the dp list with 1
